836c13dbfedd566a1c7a769c0ee8a693ed3e89b6
roamelia <115588053+roamelia@users.noreply.github.com>
2022-12-13 02:23:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const fill = document.querySelectorAll(".draggable");
+const empties = document.querySelectorAll(".empty");
+
+//fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend',dragEnd);
+
+// loop through empties and call drag events
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+// Drag Functions
+function dragStart() {
+    this.className += ' hold';
+   setTimeout(() => this.className = 'invisible', 0);
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+   e.preventDefault();
+   this.className += ' hovered';
+}
+function dragLeave() {
+    this.className = 'empty';
+}
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+}
